64f1cdf37e191c1d460d052524e60340c4f9a7ee
Vassiliy Girichev <greymem@gmail.com>
2026-09-20 20:37:42 +0300

    `xardbaiz.im` allowed domain

--- src/http.server.ts
+++ src/http.server.ts
@@ -11,7 +11,9 @@ import 'dotenv/config';
 
 if (process.env.MCP_SERVER_HTTP_TRANSPORT_ENABLED === 'true') {
     const app = createMcpExpressApp({
-        allowedHosts: ['localhost', '127.0.0.1', 'invest-idea-api.onrender.com', 'onrender.com', 'o6vsfcmvkoj3ebdrzanxm4rb.92.5.25.31.sslip.io', '92.5.25.31']
+        allowedHosts: ['localhost', '127.0.0.1', 'invest-idea-api.onrender.com', 'onrender.com',
+            'finance.xardbaiz.im', '.xardbaiz.im', '*.xardbaiz.im',
+            'o6vsfcmvkoj3ebdrzanxm4rb.92.5.25.31.sslip.io', '92.5.25.31']
     });
     const expressPort = process.env.PORT ?? 3000;
     const server = getServer();
